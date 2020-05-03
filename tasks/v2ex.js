53db5df2c05fe480891dcaeba1502a4f499a4200
limichange <limichange@hotmail.com>
2020-05-03 14:50:37 +0800

    feat: v2ex wait daily mission success

--- tasks/v2ex.js
+++ tasks/v2ex.js
@@ -4,6 +4,9 @@ const config = {
   async run(page) {
     await page.waitFor(1000)
     await page.click('#Main > div.box > div:nth-child(2) > input')
+    await page.waitForSelector(
+      '#Main > div.box > div:nth-child(2) > table > tbody > tr > td > span'
+    )
     await page.waitFor(1000)
   },
 }

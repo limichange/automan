d3458167e971abd7a0fe258f107aec5173930751
limichange <limichange@hotmail.com>
2020-05-05 16:38:38 +0800

    fixed: v2ex don't wait result, just wait 5 second

--- tasks/v2ex.js
+++ tasks/v2ex.js
@@ -4,10 +4,7 @@ const config = {
   async run(page) {
     await page.waitFor(1000)
     await page.click('#Main > div.box > div:nth-child(2) > input')
-    await page.waitForSelector(
-      '#Main > div.box > div:nth-child(2) > table > tbody > tr > td > span'
-    )
-    await page.waitFor(1000)
+    await page.waitFor(5000)
   },
 }
 
